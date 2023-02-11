fac6888a1788257f5561fa68917fdb4330ce6047
Savvy48 <viraj.patel482@gmail.com>
2023-02-11 11:03:27 +0530

    Creating the chess board

--- src/const.py
+++ src/const.py
@@ -5,4 +5,4 @@ HEIGHT = 800
 # Board dimensions 
 ROWS = 8
 COLS = 8
-
+SQSIZE = WIDTH // COLS
--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
+import pygame
+import sys
+
+from const import *
+from game import *
+
 class Main:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+        pygame.display.set_caption('Chess')
+        self.game = Game()
     
     def mainloop(self):
-        pass
+        
+        while True:
+            
+            self.game.show_bg()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                
+            pygame.display.update()
     
 main = Main()
 main.mainloop()

69db5ed9416afaab9bd5887b83cadb1e0a3f441f
Savvy48 <viraj.patel482@gmail.com>
2023-02-12 11:23:57 +0530

    Creating pieces

---
+++ src/board.py
@@ -0,0 +1,18 @@
+from const import *
+from square import Square
+
+class Board:
+    
+    def __init__(self) -> None:
+        self.squares = []
+        self._create()
+    
+    def _create(self):
+        self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
+        
+        for row in range(ROWS):
+            for col in range(COLS):
+                self.squares[row][col] = Square(row,col)
+    
+    def _add_pieces(self, color):
+        pass
---
+++ src/piece.py
@@ -0,0 +1,43 @@
+class Piece:
+    
+    def __init__(self, name, color, value, texture, texture_rect) -> None:
+        self.name = name
+        self.color = color
+        value_sign = 1 if color == 'white' else -1
+        self.value = value = value_sign
+        self.set_texture()
+        self.texture_rect = texture_rect
+        
+    def set_texture(self):
+        pass
+        
+class Pawn(Piece):
+    
+    def __init__(self, color) -> None:
+        self.dir = -1 if color == 'white' else 1
+        super().__init__('pawn', color, 1.0)
+
+class Knight(Piece):
+    
+    def __init(self, color):
+        super().__init__('knight', color, 3.0)
+        
+class Bishop(Piece):
+    
+    def __init(self, color):
+        super().__init__('knight', color, 3.001)
+        
+class Rook(Piece):
+    
+    def __init(self, color):
+        super().__init__('rook', color, 5.0)
+        
+class Queen(Piece):
+    
+    def __init(self, color):
+        super().__init__('queen', color, 9.0)
+        
+class King(Piece):
+    
+    def __init(self, color):
+        super().__init__('king', color, 10000.0)
